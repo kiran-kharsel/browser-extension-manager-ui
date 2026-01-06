c2824914fcb0cc09abb3886b3bcb5436d291ea37
kiran-kharsel <kirankharsel@gmail.com>
2026-01-06 12:25:53 +0530

    add extensions data array

---
+++ main.js
@@ -0,0 +1,69 @@
+// data array
+const extensions = [
+    {
+        id: 1,
+        icon: <i class="fa-brands fa-searchengin"></i>,
+        title: 'devlens',
+        desc: 'quickly inspect page layout and visualise element boundaries.'
+    },
+    {
+        id: 2,
+        icon: <i class="fa-solid fa-file-code"></i>,
+        title: 'stylespy',
+        desc: 'instantly analyze and copy CSS fromany webpage element'
+    },
+    {
+        id: 3,
+        icon: <i class="fa-brands fa-gauge-high"></i>,
+        title: 'speedboost',
+        desc: 'optimize browser resource usage to accelarate page loading'
+    },
+    {
+        id: 4,
+        icon: <i class="fa-brands  fa-wand-magic-sparkles"></i>,
+        title: 'JSONwizard',
+        desc: 'formats, validates and pretifies json responses in-browser'
+    },
+    {
+        id: 5,
+        icon: <i class="fa-brands fa-tablet"></i>,
+        title: 'tabmaster pro',
+        desc: 'organise browser tabs into groups ans sessions'
+    },
+    {
+        id: 6,
+        icon: <i class="fa-brands fa-expand"></i>,
+        title: 'viewportbuddy',
+        desc: 'simulates various screen resolutions directly withi the browser'
+    },
+    {
+        id: 7,
+        icon: <i class="fa-brands fa-grip"></i>,
+        title: 'markup notes',
+        desc: 'enable annotations and notes directly onto webpage'
+    },
+    {
+        id: 8,
+        icon: <i class="fa-brands fa-file-pen"></i>,
+        title: 'gridguides',
+        desc: 'overlay customizable grids an alignment guids for webpage.'
+    },
+    {
+        id: 9,
+        icon: <i class="fa-brands fa-swatchbook"></i>,
+        title: 'palette picker',
+        desc: 'instantly extract color from any webpage'
+    },
+    {
+        id: 10,
+        icon: <i class="fa-brands fa-link"></i>,
+        title: 'linkchecker',
+        desc: 'scans an highkights broken link on web page'
+    },
+    {
+        id: 11,
+        icon: <i class="fa-brands fa-terminal"></i>,
+        title: 'console plus',
+        desc: 'enhanced dev console with advanced filtring an logging'
+    },
+]
